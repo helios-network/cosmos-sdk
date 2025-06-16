6589aff053b60359e7103311c830207a53a42b63
jguyet <jguyet.weoohh@gmail.com>
2025-06-16 17:47:28 +0200

    dont panic init from genesis file

--- x/bank/keeper/genesis.go
+++ x/bank/keeper/genesis.go
@@ -48,9 +48,9 @@ func (k BaseKeeper) InitGenesis(ctx context.Context, genState *types.GenesisStat
 	}
 	totalSupply := totalSupplyMap.ToCoins()
 
-	if !genState.Supply.Empty() && !genState.Supply.Equal(totalSupply) {
-		panic(fmt.Errorf("genesis supply is incorrect, expected %v, got %v", genState.Supply, totalSupply))
-	}
+	// if !genState.Supply.Empty() && !genState.Supply.Equal(totalSupply) {
+	// 	panic(fmt.Errorf("genesis supply is incorrect, expected %v, got %v", genState.Supply, totalSupply))
+	// }
 
 	for _, supply := range totalSupply {
 		k.setSupply(ctx, supply)
