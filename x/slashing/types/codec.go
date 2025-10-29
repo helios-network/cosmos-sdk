88b8ae1bf807862550acf9d25bc6b66af0282567
jguyet <jguyet.weoohh@gmail.com>
2025-10-29 14:11:00 +0100

    clean unused

--- x/slashing/types/codec.go
+++ x/slashing/types/codec.go
@@ -6,7 +6,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 )
 
 // RegisterLegacyAminoCodec registers concrete types on LegacyAmino codec
@@ -14,7 +13,6 @@ func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(Params{}, "cosmos-sdk/x/slashing/Params", nil)
 	legacy.RegisterAminoMsg(cdc, &MsgUnjail{}, "cosmos-sdk/MsgUnjail")
 	legacy.RegisterAminoMsg(cdc, &MsgUpdateParams{}, "cosmos-sdk/x/slashing/MsgUpdateParams")
-	legacy.RegisterAminoMsg(cdc, &SlashingProposal{}, "cosmos-sdk/x/slashing/SlashingProposal")
 }
 
 // RegisterInterfaces registers the interfaces types with the Interface Registry.
@@ -22,16 +20,6 @@ func RegisterInterfaces(registry types.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil),
 		&MsgUnjail{},
 		&MsgUpdateParams{},
-	)
-
-	registry.RegisterInterface(
-		"cosmos.gov.v1beta1.Content",
-		(*govtypes.Content)(nil),
-		&SlashingProposal{},
-	)
-
-	registry.RegisterImplementations((*govtypes.Content)(nil),
-		&SlashingProposal{},
 	)
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
